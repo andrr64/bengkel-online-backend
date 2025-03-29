e2624ff3927a4498a0382fcddf2e2c8f6da498e5
andrr64 <derzandrr03@gmail.com>
2025-03-30 01:05:38 +0700

    feat: Add PostgreSQL and Redis configuration parameters to settings

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,13 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
-
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    REDIS_URL: str
+    REDIS_HOST: str
+    REDIS_PORT: str
+    
     class Config:
         env_file = ".env"
 
